3bb991574af7927756d2f0f72c48c59c6e965f05
Rohan Banerjee <rohan.b.banerjee@gmail.com>
2023-03-29 16:34:00 -0400

    add external code to print metrics for best model

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-## Author: Rohan Banerjee
+## Author: Rohan Banerjee (and Prishita Ray)
 ## Utilities file (contains useful methods for CarRacing)
 
 def check_if_car_on_grass(car):
@@ -22,3 +22,48 @@ def check_if_car_on_grass(car):
             # wheel is on the grass (not in contact with any tiles, either road or obstacle)
             return True
     return False
+
+def evaluate_best_model(best_model, eval_env, num_episodes=500):
+    """
+    Evaluates a policy on an evaluation CarRacing environment.
+    (based on code from: Prishita Ray)
+
+    Prints the following metrics:
+    - Episode scores (for every episode)
+    - Average number of tiles covered (averaged over all episodes)
+    - Average time taken (averaged over all episodes)
+    - Average proportion of time spent on grass (averaged over all episodes)
+
+    Args:
+        best_model (stable_baselines3.PPO): best policy
+        eval_env (car_racing.CarRacing): evaluation environment
+    """
+    # To calculate number of tiles covered and time taken in default environment
+    frames = []
+    tiles=0
+    times=0
+    total_road_or_obstacle_timesteps = 0
+    total_grass_timesteps = 0
+    for episode in range(1, num_episodes+1):
+        obs = eval_env.reset()  #state = env.reset()
+        done = False
+        score = 0
+        info={}
+        while not done:
+            # Check if car is on the grass
+            if check_if_car_on_grass(eval_env.car):
+                total_grass_timesteps += 1
+            else:
+                total_road_or_obstacle_timesteps += 1
+            # frames.append(env.render(mode='rgb_array'))
+            action , _ = best_model.predict(obs.copy())
+            obs, reward, done, info = eval_env.step(action)
+            score += reward
+        tiles+=eval_env.tile_visited_count
+        times+=eval_env.t
+        print("Episode:{} Score:{}".format(episode,score))
+    print("Number of tiles:",tiles/num_episodes)
+    print("Time taken:",times/num_episodes)
+    print("Proportion of time spent on grass:", \
+          total_grass_timesteps/(total_grass_timesteps+total_road_or_obstacle_timesteps))
+    eval_env.close()
